71c30772b125cd7abee25ea30a988fc88078c407
Sonwabile <219267189@mycput.ac.za>
2021-06-08 10:54:14 +0100

    Added Test Package

---
+++ src/test/java/za/ac/cput/Factory/PrescriptionFactoryTest.java
@@ -0,0 +1,14 @@
+/*
+    PrescriptionFactoryTest.java
+    TestFactory for the PrescriptionFactoryTest
+    Author: Sonwabile Gxoyiya (219267189)
+    Date: 8 June 2021
+ */
+
+package za.ac.cput.Factory;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PrescriptionFactoryTest {
+
+}

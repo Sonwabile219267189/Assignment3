e4abb2ba761d7f285d025fe3f7875d407ab48498
Sonwabile <219267189@mycput.ac.za>
2021-06-08 10:38:23 +0100

    Factory package added

---
+++ src/main/java/za/ac/cput/Factory/PrescriptionFactory.java
@@ -0,0 +1,11 @@
+/*
+    PrescriptionFactory.java
+    Factory for the Prescription class
+    Author: Sonwabile Gxoyiya (219267189)
+    Date: 8 June 2021
+ */
+
+package za.ac.cput.Factory;
+
+public class PrescriptionFactory {
+}

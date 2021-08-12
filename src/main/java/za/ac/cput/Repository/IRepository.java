ebdb4e8972eb1fd9cb2f048f3e091b8e4b266283
Sonwabile <219267189@mycput.ac.za>
2021-08-12 23:03:11 +0100

    Controller and Service Packages added

--- src/main/java/za/ac/cput/Repository/IRepository.java
+++ src/main/java/za/ac/cput/Repository/IRepository.java
@@ -3,10 +3,10 @@ package za.ac.cput.Repository;
 import java.util.List;
 
 public interface IRepository<T, ID> {
-    public T create();
+    public T create(T t);
 
-    public T read();
-    public T update();
-    public T delete();
+    public T read(ID id);
+    public T update(T t);
+    public boolean delete(ID id);
 
 }

a316795f4f2072334df048f6f6d4ba57c088431c
Sonwabile <219267189@mycput.ac.za>
2021-10-18 12:00:10 +0100

    Added code to SecurityConfig file.

--- src/main/java/za/ac/cput/Security/SecurityConfig.java
+++ src/main/java/za/ac/cput/Security/SecurityConfig.java
@@ -1,22 +1,47 @@
 
 
 package za.ac.cput.Security;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
+        auth.inMemoryAuthentication()
+                .withUser("adminUser")
+                .password(passwordEncoder().encode(""))
+                .roles("ADMIN")
+                .and()
+                .withUser("")
+                .password(passwordEncoder().encode(""))
+                .roles("USER");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic()
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST,"").hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET,"").hasRole("USER")
+                .and()
+                .csrf().disable()
+                .formLogin().disable();
+    }
 
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
